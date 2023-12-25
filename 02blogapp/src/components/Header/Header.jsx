de61827eaf803cb434b380dc871747d1744889b4
Jeel Patel <patel.jeel@northeastern.edu>
2023-12-25 00:02:44 -0500

    Application Components created

---
+++ 02blogapp/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from "../.../appwrite/auth.js";
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
